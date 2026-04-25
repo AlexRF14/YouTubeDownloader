da5cf9d072079db5501c1d9470b6eafac646b057
alejandro.rodriguezferrer <alejandro.rodriguezferrer@usp.ceu.es>
2026-04-25 18:15:20 +0200

    Now should work on Windows too

--- yt_downloader.py
+++ yt_downloader.py
@@ -1,17 +1,31 @@
+import shutil
 from yt_dlp import YoutubeDL
 
 def download_video(url):
+    node_path = shutil.which("node")
+    
     ydl_opts = {
         "outtmpl": "downloads/%(title)s [%(id)s].%(ext)s",
         "format": "bestvideo*+bestaudio/best",
-        "js_runtimes": {"node": {}},
         "merge_output_format": "mp4"  # optional: merge into mp4
     }
+    
+    if node_path:
+        print("Node.js on the device")
+        ydl_opts["js_runtimes"] = {"node": {}}
+    else:
+        print("Node.js not on the device, slower donwload")
 
-    with YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-
+    try:
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+    except Exception as e:
+        print(f"Error: {e}")
+        
 if __name__ == "__main__":
     url = input("Enter YouTube URL: ").strip()
-    download_video(url)
-    print("Download finished.")
+    if url:
+        download_video(url)
+        print("Download finished.")
+    else:
+        print("URL no válida.")
